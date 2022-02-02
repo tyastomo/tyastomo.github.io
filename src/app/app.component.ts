d74f2bea0515964d764f5bed984bfd6ab0a44af8
fahmi.t <fahmi.t@halosis.co.id>
2022-02-02 09:46:49 +0700

    profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,124 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-ang';
+  faGithub = faGithub;
+  faLinkedinIn = faLinkedinIn;
+  faFacebookF = faFacebookF;
+  faWhatsapp = faWhatsapp;
+  faInstagram = faInstagram;
+  faDiscord = faDiscord;
+  Experiences = [
+    {
+      'id': 1,
+      'office': 'PT Hasil Sukses Indonesia',
+      'position': 'Developer',
+      'year': '2022 - Now'
+    },
+    {
+      'id': 2,
+      'office': 'PT Prima Mandiri Komunikasi',
+      'position': 'Staff Developer',
+      'year': '2020 - 2022'
+    },
+    {
+      'id': 3,
+      'office': 'PT Solusi Digital Industri',
+      'position': 'Frontend Developer',
+      'year': '2020'
+    },
+    {
+      'id': 4,
+      'office': 'PT Sisfomedika',
+      'position': 'Web Designer',
+      'year': '2017 - 2019'
+    },
+  ];
+  Skills = [
+    {
+      'id': 1,
+      'name': 'Backend',
+      'sets': [
+        {
+          'id': 1,
+          'name': 'PHP'
+        },
+        {
+          'id': 2,
+          'name': 'Laravel Framework'
+        },
+        {
+          'id': 3,
+          'name': 'MySQL'
+        },
+        {
+          'id': 4,
+          'name': 'PostgreSQL'
+        },
+      ], 
+    },
+    {
+      'id': 2,
+      'name': 'Frontend',
+      'sets': [
+        {
+          'id': 1,
+          'name': 'Bootstrap CSS'
+        },
+        {
+          'id': 2,
+          'name': 'VueJS'
+        },
+        {
+          'id': 3,
+          'name': 'NuxtJS'
+        },
+        {
+          'id': 4,
+          'name': 'Angular'
+        },
+        {
+          'id': 5,
+          'name': 'JQuery'
+        },
+        {
+          'id': 6,
+          'name': 'TailwindCSS'
+        },
+        {
+          'id': 7,
+          'name': 'CSS 3'
+        },
+      ], 
+    },
+    {
+      'id': 3,
+      'name': 'Others',
+      'sets': [
+        {
+          'id': 1,
+          'name': 'Git'
+        },
+        {
+          'id': 2,
+          'name': 'Linux Server'
+        },
+        {
+          'id': 3,
+          'name': 'Wordpress'
+        },
+        {
+          'id': 4,
+          'name': 'Figma'
+        },
+        {
+          'id': 5,
+          'name': 'Illustrator'
+        },
+        {
+          'id': 6,
+          'name': 'Photoshop'
+        },
+      ], 
+    }
+  ];
 }
